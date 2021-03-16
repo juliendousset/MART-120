4a02c37a97300aaf673d465ead6ad3057fa5a39e
juliendousset <77361261+juliendousset@users.noreply.github.com>
2021-03-15 18:24:35 -0700

    Add files via upload

---
+++ Homework9/Homework9.js
@@ -0,0 +1,32 @@
+function setup()
+{
+    createCanvas(500, 600);
+}
+
+function draw()
+{
+    background(150,200,100);
+    fill(150,150,150);
+    ellipse(190,140,40,100);
+    ellipse(310,140,40,100);
+    fill(220,200,180);
+    circle(250,100,150);
+    fill(100,150,200);
+    rect(210,310,25,150);
+    rect(260,310,25,150);
+    rect(185,180,25,150);
+    rect(285,180,25,150);
+    rect(210,165,75,150);
+    fill(200,180,160);
+    triangle(255, 100, 230, 100, 270, 120);
+    line(260, 50, 290, 50);
+    line(210, 50, 240, 50);
+    circle(225,80,35);
+    circle(275,80,35);
+    point(225, 80);
+    point(275, 80);
+    ellipse(250,135,70,25);
+    fill(0,0,0)
+    textSize(20);
+    text('Julien Dousset', 50,550);
+}
