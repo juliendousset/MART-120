80149ff705d95630885590fbdba243f7f1d3d6f7
juliendousset <77361261+juliendousset@users.noreply.github.com>
2021-04-26 11:43:29 -0700

    Add files via upload

--- FinalProject/js/main.js
+++ FinalProject/js/main.js
@@ -1,19 +1,15 @@
 // Sets Canvas Size
-var canvasheight = window.innerHeight-110;
-var canvaswidth = window.innerWidth-20;
+var canvasheight = window.innerHeight - 20;
+var canvaswidth = window.innerWidth - 25;
 
 // Create a random color at the start
 var r = GetRandomNumber(255);
 var g = GetRandomNumber(255);
 var b = GetRandomNumber(255);
+// Random Color for the Bubbles
 var r2 = 255 - GetRandomNumber(20);
 var g2 = 255 - GetRandomNumber(20);
 var b2 = 255 - GetRandomNumber(20);
-
-// Variables for Pointer Location
-var x = canvaswidth/10;
-var y = window.innerHeight/2;
-var diameter = 10;
 
 // Create Variables for the Graph
 var InitialInvestment;
@@ -31,22 +27,16 @@ var TimeHorizon;
 var RectWidtht;
 var InterestRate;
 var RectXs = [];
-RectXs [0] = 0;
 var InflationRate = 0.02;
 var Scale = 1;
 var Middle = canvasheight-(canvasheight/3);
-
-// For Background
-var MyXs = [];
-var MyYs = [];
-var MyDiameters = [];
-var squarexspeed = [];
-var squareyspeed = [];
+var FormFilled = "no";
 
 function setup()
 {
     createCanvas(canvaswidth,canvasheight);
     
+    // Create Location for the Bubbles
     for(var i = 0; i < 10; i++)
     {
         MyXs[i] = GetRandomNumber(canvaswidth);
@@ -57,19 +47,39 @@ function setup()
 
 function draw()
 {
-    Resize();
-
     background(255);
 
-    BackgroundCreation();
+    Resize();
+
+    BackgroundBubbles();
 
     CreateGraph();
 
     CreatePointer();
 
-    PointerInstructions();
-
     PointerMovement();
+}
+
+function Resize()
+{
+    if (document.getElementById("scale").value == "Pixel")
+    {
+        var canvasheight = CompoundInterest(TimeHorizon - 1) + (CompoundInterest(TimeHorizon - 1) * (1/3));
+        var canvaswidth = window.innerWidth - 25;
+        resizeCanvas(canvaswidth, canvasheight);
+        RectWidth = canvaswidth/TimeHorizon;
+        Middle = canvasheight-(canvasheight/3);
+        Scale = 1;
+    }
+    if (document.getElementById("scale").value == "Scaled")
+    {
+        var canvasheight = window.innerHeight - 20;
+        var canvaswidth = window.innerWidth - 25;
+        resizeCanvas(canvaswidth, canvasheight);
+        RectWidth = canvaswidth/TimeHorizon;
+        Middle = canvasheight-(canvasheight/3);
+        Scale = Middle/CompoundInterest(TimeHorizon - 1);
+    }
 }
 
 function GetRandomNumber(number)
@@ -77,37 +87,7 @@ function GetRandomNumber(number)
     return Math.floor(Math.random()*number);
 }
 
-function BackgroundCreation()
-{
-    fill(r2,g2,b2);
-    for(var i = 0; i < MyXs.length; i++)
-    {
-        circle(MyXs[i],MyYs[i],MyDiameters[i]);
-        squarexspeed[i] = Math.floor(Math.random() * 2);
-        squareyspeed[i] = Math.floor(Math.random() * 2);
-        // Obstacle Speed
-        MyXs[i] += squarexspeed[i];
-        MyYs[i] += squareyspeed[i];
-        // Checks if Obstacles have hit a border and moves them to the other side
-        if (MyXs[i] > window.innerWidth-20)
-        {
-            MyXs[i] = 0;
-        }
-        if (MyXs[i] < 0)
-        {
-            MyXs[i] = window.innerWidth-20;
-        }
-        if (MyYs[i] > window.innerHeight-110)
-        {
-            MyYs[i] = 0;
-        }
-        if (MyYs[i] < 0)
-        {
-            MyYs[i] = window.innerHeight-110;
-        }
-    }
-}
-
+// Functions to get the calculations for the Graph
 function CompoundInterest(time)
 {
     return Math.floor((InitialInvestment * (((1+ (InterestRate/Compounds)) ** (Compounds * time)))));
@@ -125,9 +105,9 @@ function Inflation(time)
 
 function CreateGraph()
 {
-    if (InitialInvestment != "", Compounds != "", TimeHorizon != "", InterestRate != "")
+    if (FormFilled == "yes")
     {
-    // Creates the Graph
+        // Creates the Graph
         for(var i = 1; i < TimeHorizon; i++)
         {
             // Determine the Xs of the Rectangles
@@ -142,90 +122,39 @@ function CreateGraph()
             // Input Compounded Graphed
             fill(r,g,b);
             rect(RectXs[i], Middle-CompoundedMoney[i], RectWidth, CompoundedMoney[i]);
+            fill(0);
             text(CompoundedMoneyValue[i], RectXs[i], (Middle - CompoundedMoney[i]) - 5);
             // Lost to Inflation
             fill(50);
             rect(RectXs[i], Middle-MoneyLost[i], RectWidth, MoneyLost[i]);
+            fill(0);
             text(MoneyLostValue[i], RectXs[i], (Middle - MoneyLost[i]) + 15);
             // Adjusted For Inflation
             fill(b,r,g);
             rect(RectXs[i], Middle-InflationCompounded[i], RectWidth, InflationCompounded[i]);
+            fill(0);
             text(InflationCompoundedValue[i], RectXs[i], (Middle - InflationCompounded[i]) - 5);
-            // Initial Investment Rectangle
-            fill(50);
-            rect(RectXs[0], Middle-CompoundedMoney[0], RectWidth, CompoundedMoney[0]);
-            text(CompoundedMoneyValue[0], RectXs[0], (Middle - CompoundedMoney[0]) - 5);
             // Text to say what year it is
             fill(0);
-            text(i, RectXs[i], (window.innerHeight-110) - 10);
-            text(0, 0, canvasheight - 10);
+            text(i, RectXs[i], (window.innerHeight - 20) - 10);
         }
+        // Year 0 Text
+        text(0, 0, (window.innerHeight - 20) - 10);
+        // Initial Investment Rectangle
+        fill(50);
+        rect(RectXs[0], Middle-CompoundedMoney[0], RectWidth, CompoundedMoney[0]);
+        text(CompoundedMoneyValue[0], RectXs[0], (Middle - CompoundedMoney[0]) - 5);
+        // Graph Instructions
+        textSize(15);
+        fill(50);
+        square(RectXs[0] + 5, Middle + 85, 15);
+        fill(b,r,g);
+        square(RectXs[0] + 5, Middle + 65, 15);
+        fill(r,g,b);
+        square(RectXs[0] + 5, Middle + 45, 15);
+        fill(0);
+        text("Compounded Money", RectXs[0] + 25, Middle + 60);
+        text("Compounded Money Adjusted for 2% Inflation", RectXs[0] + 25, Middle + 80);
+        text("Value Lost to 2% Inflation", RectXs[0] + 25, Middle + 100);
     }
-}
-
-function CreatePointer()
-{
-    fill(g,b,r);
-    circle(x,y,diameter);
-    textSize(30);
-}
-
-function PointerInstructions()
-{
-    textSize(15);
-    fill(50);
-    text("Value Lost to Inflation", 0, 20);
-    fill(b,r,g);
-    text("Compounded Money Value Adjusted for 2% Inflation", 0, 40);
-    fill(r,g,b);
-    text("Compounded Money", 0, 60);
-}
-
-function PointerMovement()
-{
-    fill(g,b,r);
-    text("Change: " + Math.floor((Middle - y) /  Scale), x - 40, y + 25);
-
-    // Regular WASD
-    if(keyIsDown(68))
-    {
-        x+=10;
-    }
-    if(keyIsDown(65))
-    {
-        x-=10;
-    }
-    if(keyIsDown(87))
-    {
-        y-=10;
-    }
-    if(keyIsDown(83))
-    {
-        y+=10;
-    }
-
-    // Checks to make sure pointer is not out of bounds
-    if (x > canvaswidth)
-    {
-        x = 0;
-    }
-    if (x < 0)
-    {
-        x = canvaswidth;
-    }
-    if (y > canvasheight)
-    {
-        y = 0;
-    }
-    if (y < 0)
-    {
-        y = canvasheight;
-    }
-}
-
-// Move the pointer to the position where the mouse was clicked
-function mouseClicked()
-{
-    x = mouseX;
-    y = mouseY;
 }
